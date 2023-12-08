502676367e941ff9f99ddc5a7cd3a0c45662f3d9
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-08 14:40:58 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,52 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main id="main" className="col-md-6">
+      <h2>ホーム</h2>
+      <div className="row">
+        <form action="" method="post">
+          <textarea className="form-control" name="message" placeholder="いまどうしてる？"></textarea>
+
+          <div className="mt-3 mb-3 text-center">
+            <button className="btn btn-primary rounded-pill w-25">つぶやく</button>
+          </div>
+        </form>
+      </div>
+
+      <div className="row">
+        <article className="tweet d-flex">
+          <div className="profile-image">
+            <img src="images/me.png" alt="" />
+          </div>
+
+          <div className="tweet-body">
+            <div className="tweet-user">
+              <span className="fw-bold">@YSE</span>
+              <span className="ms-1 text-secondary">30分前</span>
+            </div>
+            <div className="tweet-text">
+              東京駅にいます！
+            </div>
+            <nav className="tweet-nav">
+              <ul className="d-flex mt-2">
+                <li>
+                  <a href="#"><img src="svg/bubble.svg" /></a>
+                </li>
+                <li>
+                  <a href="#"><img src="svg/loop.svg" /></a>
+                </li>
+                <li>
+                  <a href="#"><img src="svg/heart.svg" /></a>
+                </li>
+                <li>
+                  <a href="#"><img src="svg/trash.svg" /></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </article>
+      </div>
+    </main>
   );
 }
 
